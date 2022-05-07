94ceb18654804b4c38baf0c206a1a9385d871075
Hendrik Mans <hendrik@mans.de>
2022-05-07 15:52:27 +0200

    Set up r3f

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Canvas } from "@react-three/fiber";
 import { FC } from "react";
+import { Particles } from "./Particles";
 
-export const App: FC = () => <p>hi</p>;
+export const App: FC = () => (
+  <Canvas>
+    <Particles />
+  </Canvas>
+);
---
+++ src/Particles.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const Particles: FC = () => {
+  return null;
+};
