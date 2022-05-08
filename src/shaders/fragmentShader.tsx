fe944253d1ea84a371c703cd88084c0f678fce4b
Hendrik Mans <hendrik@mans.de>
2022-05-08 12:16:19 +0200

    Cleanup

--- src/shaders/fragmentShader.tsx
+++ src/shaders/fragmentShader.tsx
@@ -4,6 +4,10 @@ varying vec3 v_uv;
 
 void main()
 {
-  float depth = gl_FragCoord.z / gl_FragCoord.w;
-  gl_FragColor = vec4(v_uv.z, 1.0, 1.0, v_uv.z);
+  gl_FragColor = vec4(
+    v_uv.z,
+    1.0,
+    1.0,
+    v_uv.z
+  );
 }`;
