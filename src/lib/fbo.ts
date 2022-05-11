f0f05d01034ca1031d9a0ade7eee69fd0d5bf6c5
Hendrik Mans <hendrik@mans.de>
2022-05-11 18:25:33 +0200

    Create material and scene

--- src/lib/fbo.ts
+++ src/lib/fbo.ts
@@ -1,23 +1,35 @@
 import { insideSphere } from "randomish";
 import {
+  AdditiveBlending,
   BufferAttribute,
   BufferGeometry,
   DataTexture,
   FloatType,
+  Mesh,
   NearestFilter,
+  OrthographicCamera,
   RGBAFormat,
+  Scene,
+  ShaderMaterial,
   WebGLRenderTarget,
 } from "three";
+import simulationFragmentShader from "../shaders/simulation.frag";
+import simulationVertexShader from "../shaders/simulation.vert";
 
 export class FBO {
   public texture!: DataTexture;
   private geometry!: BufferGeometry;
   private renderTargets!: [WebGLRenderTarget, WebGLRenderTarget];
+  private scene!: Scene;
+  private camera!: OrthographicCamera;
+  private material!: ShaderMaterial;
 
   constructor(public width: number, public height: number) {
     this.createTexture();
     this.createGeometry();
     this.createRenderTargets();
+    this.createMaterial();
+    this.createScene();
   }
 
   private createTexture() {
@@ -70,5 +82,51 @@ export class FBO {
       });
 
     this.renderTargets = [make(), make()];
+  }
+
+  private createMaterial() {
+    this.material = new ShaderMaterial({
+      vertexShader: simulationVertexShader,
+      fragmentShader: simulationFragmentShader,
+      uniforms: {
+        u_positions: { value: this.texture },
+        u_time: { value: 0.0 },
+      },
+      transparent: true,
+      blending: AdditiveBlending,
+    });
+  }
+
+  private createScene() {
+    this.scene = new Scene();
+    this.camera = new OrthographicCamera(-1, 1, 1, -1, 1 / Math.pow(2, 53), 1);
+
+    const geometry = new BufferGeometry();
+
+    geometry.setAttribute(
+      "position",
+      new BufferAttribute(
+        new Float32Array([
+          -1, -1, 0, 1, -1, 0, 1, 1, 0,
+
+          -1, -1, 0, 1, 1, 0, -1, 1, 0,
+        ]),
+        3
+      )
+    );
+
+    geometry.setAttribute(
+      "uv",
+      new BufferAttribute(
+        new Float32Array([
+          0, 1, 1, 1, 1, 0,
+
+          0, 1, 1, 0, 0, 0,
+        ]),
+        2
+      )
+    );
+
+    this.scene.add(new Mesh(geometry, this.material));
   }
 }
