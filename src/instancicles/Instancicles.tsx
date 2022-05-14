f9f3e3ed52604905524373f7c9673ac7f32b0430
Hendrik Mans <hendrik@mans.de>
2022-05-14 14:02:35 +0200

    Set a matrix entry

--- src/instancicles/Instancicles.tsx
+++ src/instancicles/Instancicles.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useMemo, useRef } from "react";
+import { InstancedMesh, Matrix4, Quaternion, Vector3 } from "three";
+
 export const Instancicles = () => {
+  const imesh = useRef<InstancedMesh>(null!);
+
+  useEffect(() => {
+    const position = new Vector3();
+    const quaternion = new Quaternion();
+    const scale = new Vector3(1, 1, 1);
+
+    const mat = new Matrix4().compose(position, quaternion, scale);
+    imesh.current.setMatrixAt(0, mat);
+  }, []);
+
   return (
-    <mesh>
+    <instancedMesh ref={imesh} args={[undefined, undefined, 10000]}>
       <dodecahedronBufferGeometry />
       <meshStandardMaterial color="hotpink" />
-    </mesh>
+    </instancedMesh>
   );
 };
