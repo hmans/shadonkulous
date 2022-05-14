cf0b3d0bfb6b037d9159d596f983af6bcfeba659
Hendrik Mans <hendrik@mans.de>
2022-05-14 15:10:35 +0200

    Cool!

--- src/instancicles/Instancicles.tsx
+++ src/instancicles/Instancicles.tsx
@@ -1,5 +1,5 @@
-import { useFrame } from "@react-three/fiber";
-import { useEffect, useMemo, useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { FC, useCallback, useEffect, useMemo, useRef } from "react";
 import {
   InstancedBufferAttribute,
   InstancedMesh,
@@ -10,48 +10,102 @@ import {
 } from "three";
 import CustomShaderMaterial from "three-custom-shader-material";
 import CSMImpl from "three-custom-shader-material/vanilla";
+import { isMethodSignature } from "typescript";
 import * as shader from "./shader";
 
-export const Instancicles = () => {
+export const Instancicles: FC<{ maxParticles?: number }> = ({
+  maxParticles = 10000,
+}) => {
   const imesh = useRef<InstancedMesh>(null!);
   const material = useRef<CSMImpl>(null!);
+  const { clock } = useThree();
 
   useEffect(() => {
+    /* Add some extra attributes to the instanced mesh */
+    imesh.current.geometry.setAttribute(
+      "timeStart",
+      new InstancedBufferAttribute(new Float32Array(maxParticles), 1)
+    );
+    imesh.current.geometry.setAttribute(
+      "timeEnd",
+      new InstancedBufferAttribute(new Float32Array(maxParticles), 1)
+    );
+    imesh.current.geometry.setAttribute(
+      "velocity",
+      new InstancedBufferAttribute(new Float32Array(maxParticles), 3)
+    );
+    imesh.current.geometry.setAttribute(
+      "acceleration",
+      new InstancedBufferAttribute(new Float32Array(maxParticles), 3)
+    );
+
+    imesh.current.count = 0;
+  }, [maxParticles]);
+
+  const playhead = useRef(0);
+
+  const spawnParticle = useCallback(() => {
+    console.log("spawnParticle", playhead.current, clock.elapsedTime);
+
     const position = new Vector3();
     const quaternion = new Quaternion();
     const scale = new Vector3(1, 1, 1);
 
     const mat = new Matrix4().compose(position, quaternion, scale);
-    imesh.current.setMatrixAt(0, mat);
-    imesh.current.count = 1;
+    imesh.current.setMatrixAt(playhead.current, mat);
+    imesh.current.instanceMatrix.needsUpdate = true;
 
-    /* Add some extra attributes to the instanced mesh */
-    imesh.current.geometry.setAttribute(
-      "timeStart",
-      new InstancedBufferAttribute(new Float32Array([0]), 1)
+    imesh.current.geometry.attributes.timeStart.setX(
+      playhead.current,
+      clock.elapsedTime
     );
-    imesh.current.geometry.setAttribute(
-      "timeEnd",
-      new InstancedBufferAttribute(new Float32Array([1]), 1)
+    imesh.current.geometry.attributes.timeStart.needsUpdate = true;
+
+    imesh.current.geometry.attributes.timeEnd.setX(
+      playhead.current,
+      clock.elapsedTime + 2
     );
-    imesh.current.geometry.setAttribute(
-      "velocity",
-      new InstancedBufferAttribute(new Float32Array([+3, +4, 0]), 3)
+    imesh.current.geometry.attributes.timeEnd.needsUpdate = true;
+
+    imesh.current.geometry.attributes.velocity.setXYZ(
+      playhead.current,
+      ...new Vector3().randomDirection().toArray()
     );
-    imesh.current.geometry.setAttribute(
-      "acceleration",
-      new InstancedBufferAttribute(new Float32Array([0, -4, 0]), 3)
+    imesh.current.geometry.attributes.velocity.needsUpdate = true;
+
+    imesh.current.geometry.attributes.acceleration.setXYZ(
+      playhead.current,
+      0,
+      -5,
+      0
     );
+    imesh.current.geometry.attributes.acceleration.needsUpdate = true;
+
+    /* Advance playhead */
+    imesh.current.count++;
+    playhead.current++;
+  }, []);
+
+  useEffect(() => {
+    spawnParticle();
+
+    const interval = setInterval(() => {
+      spawnParticle();
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   const uniforms = useMemo(() => ({ u_time: { value: 0 } }), []);
 
-  useFrame((_, dt) => {
-    material.current.uniforms.u_time.value += dt;
+  useFrame(() => {
+    material.current.uniforms.u_time.value = clock.elapsedTime;
   });
 
   return (
-    <instancedMesh ref={imesh} args={[undefined, undefined, 10000]}>
+    <instancedMesh ref={imesh} args={[undefined, undefined, maxParticles]}>
       <dodecahedronBufferGeometry />
       <CustomShaderMaterial
         ref={material}
--- src/instancicles/shader.ts
+++ src/instancicles/shader.ts
@@ -10,7 +10,7 @@ varying float v_timeStart;
 varying float v_timeEnd;
 
 void main() {
-  float t = u_time;
+  float t = u_time - timeStart;
 
   /* Apply velocity and acceleration */
   csm_Position += t * velocity + 0.5 * t * t * acceleration;
